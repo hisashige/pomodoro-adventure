8ca247de9ac1e5821851cfb23da2c0b1384bc900
hisashige <hisashige56@gmail.com>
2023-09-10 09:32:20 +0900

    create turborepo

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: { browser: true, es2022: true },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "react-refresh"],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
