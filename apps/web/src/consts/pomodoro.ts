650704026e434e25b54b6aa9e4df1b483777563f
hisashige <hisashige56@gmail.com>
2023-11-27 21:40:44 +0900

    chore: change importing sound files

--- apps/web/src/consts/pomodoro.ts
+++ apps/web/src/consts/pomodoro.ts
@@ -1,54 +1,53 @@
-import rainAndGuitar from '../assets/sounds/rain_and_guitar.mp3'
-import vanillaSky from '../assets/sounds/Vanilla_Sky_2.mp3'
-import arpeggioOfSeawind from '../assets/sounds/arpeggio_of_seawind.mp3'
-import atSmallCafeInHighland from '../assets/sounds/at_small_cafe_in_highland.mp3'
-import envRain from '../assets/sounds/env_rain.mp3'
-import envWave from '../assets/sounds/env_wave.mp3'
-import envCafe from '../assets/sounds/env_cafe.mp3'
-
 export const POMODORO_TIME = 1500
 
 export const SOUNDS = [
   {
     image: 'https://img.icons8.com/stickers/100/us-music.png',
     label: '雨粒とナイロンギター',
-    value: rainAndGuitar,
+    value:
+      'https://dl.dropboxusercontent.com/scl/fi/nrya2409h12f4ln6eyvd4/rain_and_guitar.mp3?rlkey=bhnpo0nqyq39xe7hz5ryiwjtj',
     description: 'リラックスに最適',
   },
   {
     image: 'https://img.icons8.com/stickers/100/kawaii.png',
     label: 'Vanilla Sky',
-    value: vanillaSky,
+    value:
+      'https://dl.dropboxusercontent.com/scl/fi/gavzugdwlzex6hntzh0uz/Vanilla_Sky_2.mp3?rlkey=ni8yu9qgyic60qx3tqu3a2k04',
     description: 'KAWAII Future Bass',
   },
   {
     image: 'https://img.icons8.com/stickers/100/water-element.png',
     label: '海風のアルペジオ',
-    value: arpeggioOfSeawind,
+    value:
+      'https://dl.dropboxusercontent.com/scl/fi/zbzzgc864guc5qprccm3a/arpeggio_of_seawind.mp3?rlkey=ad1wym6gndhpmrvi9h6jxf28b',
     description: 'エモくなりたい時に',
   },
   {
     image: 'https://img.icons8.com/stickers/100/forest.png',
     label: '高原の小さなカフェにて',
-    value: atSmallCafeInHighland,
+    value:
+      'https://dl.dropboxusercontent.com/scl/fi/5a9rpd6xtm6sm4nx1vwal/at_small_cafe_in_highland.mp3?rlkey=igz95xto61m6lnh1i77xodtde',
     description: '柔らかくて暖かいひとときを',
   },
   {
     image: 'https://img.icons8.com/stickers/100/wet.png',
     label: '環境音:雨',
-    value: envRain,
+    value:
+      'https://dl.dropboxusercontent.com/scl/fi/7lcqrfxwxevesoy88c54t/env_rain.mp3?rlkey=qgr7kbsz9xdujnq8igc7ru938',
     description: '雨の音って落ち着くよね',
   },
   {
     image: 'https://img.icons8.com/stickers/100/ocean-wave.png',
     label: '環境音:波',
-    value: envWave,
+    value:
+      'https://dl.dropboxusercontent.com/scl/fi/4ksf4na0bbmhw0cwhahfu/env_wave.mp3?rlkey=gubb4p2oxu6xipyvm0fpna000',
     description: 'さざ波に揺られて',
   },
   {
     image: 'https://img.icons8.com/stickers/100/restaurant-pickup.png',
     label: '環境音:カフェ',
-    value: envCafe,
+    value:
+      'https://dl.dropboxusercontent.com/scl/fi/oc7ekppf5q82y3wcyf6r7/env_cafe.mp3?rlkey=kv1d8c8eutv47oz2mls66d8e1',
     description: '喧騒の中で集中したい時に',
   },
 ]
